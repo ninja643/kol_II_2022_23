2dd9da2b022a4509a534622139dcc80268da4f32
Marko Milosevic <markom@niri.rs>
2023-02-06 02:38:35 +0100

    Initial commit

---
+++ src/main/java/rs/ac/ni/pmf/oop2/kol2/Player.java
@@ -0,0 +1,14 @@
+package rs.ac.ni.pmf.oop2.kol2;
+
+import lombok.Value;
+
+@Value
+public class Player
+{
+	int id;
+	String name;
+	String position;
+	String club;
+	String nationality;
+	int age;
+}
